254ea338c6f043770cfeda39269745a2ef6fad85
Vladislav Kabral <mr.kabral@mail.ru>
2025-04-09 19:49:38 +0300

    Add dependencies for Spring Web, Spring Data Jpa, Validation, Liquibase, Openfeign, Postgresql, Lombok, ModelMapper, Redis

---
+++ src/main/java/by/kabral/banktransactionmanager/BankTransactionManagerApplication.java
@@ -0,0 +1,13 @@
+package by.kabral.banktransactionmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransactionManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankTransactionManagerApplication.class, args);
+    }
+
+}
